06ab0ab50e6afad138589eef98c58762f08db4d8
Forthwith <forthwith.dn@gmail.com>
2023-03-13 06:20:23 -0500

    Fixed AffineComponent to correctly use its ancestry hierarchy for matrices.

--- GameEngine/src/gamecore/gui/gamecomponents/AffineComponent.java
+++ GameEngine/src/gamecore/gui/gamecomponents/AffineComponent.java
@@ -367,10 +367,11 @@ public abstract class AffineComponent extends JComponent
 	/**
 	 * Obtains the position of this AffineComponent.
 	 * In other words, this multiplies the vector (0,0) by the transformation to obtain a position (x,y).
+	 * @param include_parent If true, we include the parent in the transformation. If false, we omit it. If there is no parent, then the parent matrix is treated as the identiy matrix.
 	 * @return Returns the position of this AffineComponent.
 	 */
-	public Vector2d GetPosition()
-	{return M.RightMultiply(Vector2d.ZERO);}
+	public Vector2d GetPosition(boolean include_parent)
+	{return GetMatrix(include_parent).RightMultiply(Vector2d.ZERO);}
 	
 	/**
 	 * Obtains a deep copy of the matrix backing the affine transformation.
@@ -378,7 +379,7 @@ public abstract class AffineComponent extends JComponent
 	 * @return Returns a deep copy of the matrix transformation behind this AffineComponent.
 	 */
 	public Matrix2D GetMatrix(boolean include_parent)
-	{return include_parent && Parent != null ? new Matrix2D(M).RightMultiply(Parent.M) : new Matrix2D(M);}
+	{return include_parent && Parent != null ? new Matrix2D(M).RightMultiply(Parent.GetMatrix(true)) : new Matrix2D(M);}
 	
 	/**
 	 * Obtains the affine transformation applied to the contents of this component.
@@ -386,7 +387,7 @@ public abstract class AffineComponent extends JComponent
 	 * @return Returns the affine transformation applied to the contents of this component.
 	 */
 	public AffineTransform GetTransformation(boolean include_parent)
-	{return include_parent && Parent != null ? new Matrix2D(M).RightMultiply(Parent.M).ToAffine() : M.ToAffine();}
+	{return include_parent && Parent != null ? new Matrix2D(M).RightMultiply(Parent.GetMatrix(true)).ToAffine() : M.ToAffine();}
 	
 	/**
 	 * Sets the parent affine component.
